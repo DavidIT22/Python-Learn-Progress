5bf6756f09b299eb1ed179de408fece0e5403ccb
DavidIT22 <DavidWernerIT@outlook.de>
2020-12-20 13:43:38 +0100

    My Python Progress while i learn it

--- Eingabe einer Zeichenkette/eingabe_text.py
+++
@@ -1,4 +0,0 @@
-#Eingabe einer Zeichenkette
-print("Bitte einen Text eingeben")
-x = input()
-print("Ihre Eingabe:", x )
--- Verzweigung mehrfach/verweigung_mehrfach.py
+++
@@ -1,9 +0,0 @@
-x = -5
-print("x:", x)
-if x > 0:
-    print("x ist positiv")
-elif x < 0:
-    print("x ist negativ")
-else:
-    print("x ist gleich 0")
-    
