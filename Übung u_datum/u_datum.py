e515a47ce16e9b7a5b5c6ac2eed647db85dde320
DavidWernerIT22 <76394231+DavidIT22@users.noreply.github.com>
2020-12-27 19:31:34 +0100

    Update u_datum.py

--- Übung u_datum/u_datum.py
+++ Übung u_datum/u_datum.py
@@ -1,15 +1,39 @@
 #Programm zur Prüfung einer Datumsangabe
 
 print("Tag des Datums eingeben:")
-t = input()
-Tag = int(t)
+tag = int(input())
 print("Monat des Datums eingeben:")
-m = input()
-Monat = int(m)
-print("Jahr des Datums:")
-Jahr = input()
+monat = int(input())
+print("Jahr des Datums eingeben:")
+jahr = int(input())
 
-if Tag < 1 or Tag > 31:
-    print("Es handelt sich um ein falsches Datum")
-if Monat <1 or Monat > 12:
-    print("Es handelt sich um ein falsches Datum")
+
+if tag < 1 or tag > 31:
+    print("Falsches Datum")
+elif monat < 1 or monat > 12:
+    print("Falsches Datum")
+elif monat == 4 or monat == 6 or monat == 9 or monat == 11:
+    print("Letzter Tag: 30")
+    if tag < 1 or tag > 30:
+        print("Falsches Datum")
+    else:
+        print("Richtiges Datum")
+elif monat == 2:
+    if jahr%4 == 0 and jahr%100 != 0 or jahr%400 == 0:
+        print("Letzter Tag: 29")
+        if tag < 1 or tag > 29:
+            print("Falsches Datum")
+        else:
+            print("Richtiges Datum")
+    else:
+        print("Letzter Tag: 28")
+        if tag < 1 or tag > 28:
+            print("Falsches Datum")
+        else:
+            print("Richtiges Datum")
+else:
+    print("Letzter Tag: 31")
+    if tag < 1 or tag > 31:
+        print("Falsches Datum")
+    else:
+        print("Richtiges Datum")
